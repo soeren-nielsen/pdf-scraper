e792e6e3e817047b5008994b58c621382b5a0c0a
soeren-nielsen <soeren.stoevring.nielsen@outlook.com>
2021-10-12 18:04:28 +0200

    Initial commit

--- pdf-scraper.py
+++ pdf-scraper.py
@@ -1,4 +1,11 @@
-import tabula as tb
-import pandas as pd
-import re
+import tabula
 
+file = "/Users/soeren_stoevring_nielsen/Google Drive/Personligt/GitHub/pdf-scraper/document.pdf"
+
+table1 = tabula.read_pdf(file, pages=1, multiple_tables=True)
+table2 = tabula.read_pdf(file, pages=2, multiple_tables=True)
+
+table1[0]
+table2[0]
+
+print(table1[0])
