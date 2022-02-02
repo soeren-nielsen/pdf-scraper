4b3e45665865c6c5089eaf42e05c85d257a40c03
soeren-nielsen <soeren.stoevring.nielsen@outlook.com>
2022-02-02 13:42:34 +0100

    Updated pdf-scraper.py

--- pdf-scraper.py
+++ pdf-scraper.py
@@ -1,26 +1,14 @@
-from numpy import require
-import requests
+from isort import file
 import pdfplumber
 import pandas as pd
 from collections import namedtuple
 
-faktura = namedtuple('faktura', 'periode, varenummer, varegruppe, antal_kg, afdeling')
+# invoice = namedtuple('invoice', 'varenummer, varegruppe, varenavn, antal_kg, periode, afdeling')
 
-def download_file(url):
-    local_filename = url.splut('/')[-1]
+file = 'Salgsstatisk - Madservice Aalborg - Lions Park.pdf'
 
-    with requests.get(url) as r:
-        with open(local_filename, 'wb') as f:
-            f.write(r.content)
-        
-    return local_filename
-
-ap_url = 'https://drive.google.com/file/d/1ME2UbVSgn6A5KWiSwvoraBwRVVFFLvBh/view?usp=sharing'
-
-ap = download_file(ap_url)
-
-with pdfplumber.open(ap) as pdf:
-    page = pdf.pages(0)
+with pdfplumber.open(file) as pdf:
+    page = pdf.pages[0]
     text = page.extract_text()
 
 print(text)
